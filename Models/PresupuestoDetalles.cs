8b1170298393d267b02a5670f5db3e0721105fd5
leoardo4390 <tidus4390@gmail.com>
2025-10-25 19:25:27 -0300

    demas clases

---
+++ Models/PresupuestoDetalles.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class PresupuestoDetalle
+{
+    public Productos Producto { get; set; }
+    public int Cantidad { get; set; }
+
+    // public PresupuestoDetalle(Productos producto, int cantidad)
+    // {
+    //     Producto = producto;
+    //     Cantidad = cantidad;
+    // }
+
+    // public decimal Subtotal => Producto.PrecioUnitario * Cantidad;
+
+}
---
+++ Models/Presupuestos.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Presupuestos
+{
+    public int IdPresupuesto { get; set; }
+    public string NombreDestinatario { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public List<PresupuestosDetalles> Detalle { get; set; } = new List<PresupuestosDetalles>();
+
+    public decimal MontoPresupuesto()
+    {
+        return Detalle.Sum(d => d.Cantidad * d.Producto.Precio);
+    }
+
+    public decimal MontoPresupuestoConIva()
+    {
+        return MontoPresupuesto() * 1.21m;
+    }
+
+    public int CantidadProductos()
+    {
+        return Detalle.Sum(d => d.Cantidad);
+    }
+}
